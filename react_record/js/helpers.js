b736bb49ab183e1091ceb3580d4d03e96cd4266a
William Turner <williamturner@Mac-mini.lan>
2022-10-01 11:22:51 -0700

    save

--- react_record/js/helpers.js
+++
@@ -1,6 +0,0 @@
-
-    function stopAllSoundFiles(arr = this.state.audioRecordings,prop = 'song'){ // @Stop all sound recordings from playing
-        forEach((val)=>{
-           val[prop].stop()
-        })
-    }
